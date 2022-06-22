53d1657c5feff659806d2d087ec80c79f28cb9d8
Daniel Calle <danielcalleco@gmail.com>
2022-06-22 11:13:14 -0500

    First test performed with screenshot attached

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -23,3 +23,31 @@ it("Consume GET Service with query parameters", async () => {
     expect(response.status).to.equal(StatusCodes.OK);
     expect(response.config.query).to.eql(query);
 });
+
+it("Consume HEAD Service", async () => {
+    const response = await axios.head("https://httpbin.org/anything");
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.headers).to.have.property("date");
+});
+
+it("Consume PATCH Service", async () => {
+    const response = await axios.patch("https://httpbin.org/anything");
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data).to.have.property("data");
+});
+
+it("Consume PUT Service", async () => {
+    const response = await axios.put("https://httpbin.org/anything");
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data).to.have.property("url");
+});
+
+it("Consume DELETE Service", async () => {
+    const response = await axios.delete("https://httpbin.org/anything");
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data).to.have.property("method");
+});
