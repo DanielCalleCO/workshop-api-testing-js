5f64bb3fccd55d797233f9e5f6cb5ea9cd1be2a8
Daniel Calle <danielcalleco@gmail.com>
2022-06-29 16:14:22 -0500

    Workshop updating to stage 8 - step 5

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -2,11 +2,14 @@ const axios = require('axios');
 const { expect } = require('chai');
 const { StatusCodes } = require('http-status-codes');
 
-const urlBase = 'https://api.github.com/users/aperdomob';
+const urlBase = 'https://api.github.com/users';
+const urlRepos = 'https://api.github.com/repos';
+const repo = 'jasmine-json-report';
+const owner = 'aperdomob';
 
 describe('Repositories testing', () => {
   it('GET method', async () => {
-    const response = await axios.get(`${urlBase}`);
+    const response = await axios.get(`${urlBase}/${owner}`);
 
     expect(response.status).to.equal(StatusCodes.OK);
     expect(response.data.name).to.equal('Alejandro Perdomo');
@@ -15,12 +18,29 @@ describe('Repositories testing', () => {
   });
 
   it('Repositories List', async () => {
-    const response = await axios.get(`${urlBase}/repos`);
-    const finder = response.data.find((repo) => repo.name === 'jasmine-json-report');
+    const response = await axios.get(`${urlBase}/${owner}/repos`);
+    const finder = response.data.find((object) => object.name === 'jasmine-json-report');
 
     expect(response.status).to.equal(StatusCodes.OK);
     expect(finder.name).to.equal('jasmine-json-report');
     expect(finder.private).to.equal(false);
     expect(finder.description).to.equal('A Simple Jasmine JSON Report');
+  });
+
+  it('Repo download', async () => {
+    const zipFile = await axios.get(`${urlRepos}/${owner}/${repo}/zipball`);
+
+    expect(zipFile.status).to.equal(StatusCodes.OK);
+    expect(zipFile.headers['content-type']).to.equal('application/zip');
+  });
+
+  it('Readme verifying', async () => {
+    const response = await axios.get(`${urlRepos}/${owner}/${repo}/readme`);
+    const readme = 'README.md';
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data.name).to.equal(readme);
+    expect(response.data.path).to.equal(readme);
+    expect(response.data.sha).to.equal('360eee6c223cee31e2a59632a2bb9e710a52cdc0');
   });
 });
