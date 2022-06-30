2253c7e78ca00fb72bbe1139e44994e1cacad5c4
Daniel Calle <danielcalleco@gmail.com>
2022-06-30 15:34:01 -0500

    Commit with subset error for review

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -8,7 +8,7 @@ const repo = 'jasmine-json-report';
 const owner = 'aperdomob';
 
 describe('Repositories testing', () => {
-  it('GET method', async () => {
+  it('2. API consume', async () => {
     const response = await axios.get(`${urlBase}/${owner}`);
 
     expect(response.status).to.equal(StatusCodes.OK);
@@ -17,14 +17,14 @@ describe('Repositories testing', () => {
     expect(response.data.location).to.equal('Colombia');
   });
 
-  it('Repositories List', async () => {
+  it('3. Repositories List', async () => {
     const response = await axios.get(`${urlBase}/${owner}/repos`);
-    const finder = response.data.find((object) => object.name === 'jasmine-json-report');
+    const repository = response.data.find((object) => object.name === 'jasmine-json-report');
 
     expect(response.status).to.equal(StatusCodes.OK);
-    expect(finder.name).to.equal('jasmine-json-report');
-    expect(finder.private).to.equal(false);
-    expect(finder.description).to.equal('A Simple Jasmine JSON Report');
+    expect(repository.name).to.equal('jasmine-json-report');
+    expect(repository.private).to.equal(false);
+    expect(repository.description).to.equal('A Simple Jasmine JSON Report');
   });
 
   it('Repo download', async () => {
@@ -35,12 +35,25 @@ describe('Repositories testing', () => {
   });
 
   it('Readme verifying', async () => {
-    const response = await axios.get(`${urlRepos}/${owner}/${repo}/readme`);
-    const readme = 'README.md';
+    const readmeFile = await axios.get(`${urlRepos}/${owner}/${repo}/readme`);
 
-    expect(response.status).to.equal(StatusCodes.OK);
-    expect(response.data.name).to.equal(readme);
-    expect(response.data.path).to.equal(readme);
-    expect(response.data.sha).to.equal('360eee6c223cee31e2a59632a2bb9e710a52cdc0');
+    const file = await axios.get(`https://raw.githubusercontent.com/${owner}/${repo}/master/README.md`);
+
+    const verifyReadme = {
+      name: 'README.md',
+      path: 'README.md',
+      sha: '360eee6c223cee31e2a59632a2bb9e710a52cdc0'
+    };
+
+    expect(readmeFile.status).to.equal(StatusCodes.OK);
+    expect(file.status).to.equal(StatusCodes.OK);
+    expect(file).to.containSubset(verifyReadme);
+
+    // ?_______Funcionando (viejo)
+    // ? const readme = 'README.md';
+
+    // ? expect(response.data.name).to.equal(readme);
+    // ? expect(response.data.path).to.equal(readme);
+    // ? expect(response.data.sha).to.equal('360eee6c223cee31e2a59632a2bb9e710a52cdc0');
   });
 });
