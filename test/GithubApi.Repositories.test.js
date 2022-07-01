2bd98d666888d77c13c5cbe5d0208fb932647010
Daniel Calle <danielcalleco@gmail.com>
2022-07-01 11:56:26 -0500

    fix some errors, stage 8 completed

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -1,13 +1,18 @@
 const axios = require('axios');
-const { expect } = require('chai');
+const chai = require('chai');
+const md5 = require('md5');
+
+const { expect } = chai;
 const { StatusCodes } = require('http-status-codes');
+chai.use(require('chai-subset'));
 
 const urlBase = 'https://api.github.com/users';
 const urlRepos = 'https://api.github.com/repos';
+const urlDownload = 'https://raw.githubusercontent.com';
 const repo = 'jasmine-json-report';
 const owner = 'aperdomob';
 
-describe('Repositories testing', () => {
+describe('Stage 8 - GET: Repositories testing', () => {
   it('2. API consume', async () => {
     const response = await axios.get(`${urlBase}/${owner}`);
 
@@ -20,25 +25,25 @@ describe('Repositories testing', () => {
   it('3. Repositories List', async () => {
     const response = await axios.get(`${urlBase}/${owner}/repos`);
     const repository = response.data.find((object) => object.name === 'jasmine-json-report');
+    const expectedResponse = {
+      name: 'jasmine-json-report',
+      private: false,
+      description: 'A Simple Jasmine JSON Report'
+    };
 
     expect(response.status).to.equal(StatusCodes.OK);
-    expect(repository.name).to.equal('jasmine-json-report');
-    expect(repository.private).to.equal(false);
-    expect(repository.description).to.equal('A Simple Jasmine JSON Report');
+    expect(repository).to.containSubset(expectedResponse);
   });
 
-  it('Repo download', async () => {
-    const zipFile = await axios.get(`${urlRepos}/${owner}/${repo}/zipball`);
+  it('4. Repo download', async () => {
+    const zipFile = await axios.get(`${urlRepos}/${owner}/${repo}/zipball/master`);
 
     expect(zipFile.status).to.equal(StatusCodes.OK);
     expect(zipFile.headers['content-type']).to.equal('application/zip');
   });
 
-  it('Readme verifying', async () => {
-    const readmeFile = await axios.get(`${urlRepos}/${owner}/${repo}/readme`);
-
-    const file = await axios.get(`https://raw.githubusercontent.com/${owner}/${repo}/master/README.md`);
-
+  it('5. Readme verifying', async () => {
+    const readmeFile = await axios.get(`${urlRepos}/${owner}/${repo}/contents/README.md`);
     const verifyReadme = {
       name: 'README.md',
       path: 'README.md',
@@ -46,14 +51,15 @@ describe('Repositories testing', () => {
     };
 
     expect(readmeFile.status).to.equal(StatusCodes.OK);
-    expect(file.status).to.equal(StatusCodes.OK);
-    expect(file).to.containSubset(verifyReadme);
+    expect(readmeFile.data).to.containSubset(verifyReadme);
+  });
 
-    // ?_______Funcionando (viejo)
-    // ? const readme = 'README.md';
+  it('6. Readme downloading', async () => {
+    const readmeDownload = await axios.get(`${urlDownload}/${owner}/${repo}/master/README.md`);
+    const md5Readme = '3449c9e5e332f1dbb81505cd739fbf3f';
 
-    // ? expect(response.data.name).to.equal(readme);
-    // ? expect(response.data.path).to.equal(readme);
-    // ? expect(response.data.sha).to.equal('360eee6c223cee31e2a59632a2bb9e710a52cdc0');
+    expect(readmeDownload.status).to.equal(StatusCodes.OK);
+    expect(readmeDownload.headers['content-type']).to.equal('text/plain; charset=utf-8');
+    expect(md5(readmeDownload)).to.equal(md5Readme);
   });
 });
