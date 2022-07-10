5492be8bbb7aa04005842d5a129037843ea654e6
Daniel Calle <danielcalleco@gmail.com>
2022-07-10 10:51:05 -0500

    Stage 13 done

---
+++ test/GithubApi.Contract.test.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const chai = require('chai');
+const { listPublicEventsSchema } = require('../schema/ListPublicEvents.schema');
+
+const { expect } = chai;
+chai.use(require('chai-json-schema'));
+
+const urlBase = 'https://api.github.com';
+const keyPath = axios.create({
+  path: urlBase,
+  headers: { Authorization: `token ${process.env.ACCESS_TOKEN}` }
+});
+
+describe.only('Given event Github API resources', () => {
+  describe('When wanna verify the List public events', () => {
+    let response;
+
+    before(async () => {
+      response = await keyPath.get(`${urlBase}/events`);
+    });
+
+    it('then the body should have a schema', () => expect(response.data).to.be.jsonSchema(listPublicEventsSchema));
+  });
+});
