4eacc2bfb4dc571bca981c5541fb95156927f2aa
Daniel Calle <danielcalleco@gmail.com>
2022-07-13 10:23:51 -0500

    Uploading for help (#59)
    * Uploading for help
    
    * fixed and ready to rock!
    
    * 30 per page added, variable modified
    
    * 30 per page added, variable modified

---
+++ test/GithubApi.Users.test.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+const chai = require('chai');
+const { StatusCodes } = require('http-status-codes');
+
+const { expect } = chai;
+chai.use(require('chai-subset'));
+
+const urlBase = 'https://api.github.com/users';
+const keyPath = axios.create({
+  path: urlBase,
+  headers: {
+    Authorization: `token ${process.env.ACCESS_TOKEN}`
+  }
+});
+
+describe('Stage 12 - Query parameters: Users', () => {
+  let params;
+  let users;
+
+  describe('Users without modify', () => {
+    before(async () => {
+      users = await keyPath.get(`${urlBase}`);
+    });
+
+    it('Github users - 30 per page', async () => {
+      expect(users.status).to.be.eql(StatusCodes.OK);
+      expect(users.data).to.have.length(30);
+    });
+  });
+
+  describe('Modifying users per page', () => {
+    before(async () => {
+      params = {
+        per_page: 10
+      };
+      users = await keyPath.get(`${urlBase}`, { params });
+    });
+
+    it('Github users - 10 per page', async () => {
+      expect(users.status).to.be.eql(StatusCodes.OK);
+      expect(users.data).to.have.length(10);
+    });
+
+    describe('100 Users', () => {
+      before(async () => {
+        params = {
+          per_page: 100
+        };
+        users = await keyPath.get(`${urlBase}`, { params });
+      });
+      it('Github users - 100 per page', async () => {
+        expect(users.status).to.be.eql(StatusCodes.OK);
+        expect(users.data).to.have.length(100);
+      });
+    });
+  });
+});
